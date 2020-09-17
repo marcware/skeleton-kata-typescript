9b82505dd72d35eafc03f6b75b66aed71bf536b5
Marcos Palacios <m.palacios@geekshubs.com>
2020-09-17 22:56:30 +0200

    GREEN Send Three return Fizz

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,5 +1,9 @@
 class FizzBuzz {
-    static calculate( number :number): string {
+    static calculate(number: number): string {
+
+        if (number === 3) {
+            return "Fizz";
+        }
         return number.toString();
     }
 }
