1f3e7aaff831ec7b3138273def59e90e6d53ac3d
Marcos Palacios <m.palacios@geekshubs.com>
2020-09-17 23:07:01 +0200

    BLUE Magin Number

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,11 +1,12 @@
 class FizzBuzz {
     static readonly  FIZZ = 3;
+    static readonly  BUZZ = 5;
 
     static calculate(number: number): string {
 
         if (number === this.FIZZ) {
             return "Fizz";
-        }else if(number === 5){
+        }else if(number === this.BUZZ){
             return "Buzz";
         }
         return number.toString();
